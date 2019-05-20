2e5e88450d3389ffa5556688f48b862805cf72d4
communal-principle-3181 <aellis19@outlook.com>
2019-05-20 07:31:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 function takeANumber(deliLine, newCustomer)
 {
   deliLine.push(newCustomer);
+  
+  if( deliLine === null)
+  {
+    deliLine.unshift(newCustomer);
+  }
   var welcomeMessage = `Welcome, + ${name}. You are number ${newCustomer.length} in line.`;
   console.log(welcomeMessage);
 }
